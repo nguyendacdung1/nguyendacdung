f5b0eafb175180e0f42934d2417a52b846e86b6c
nguyendacdung1 <91303703+nguyendacdung1@users.noreply.github.com>
2021-09-24 11:52:40 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("trong dam gi dep bang sen\n");
+	printf("trong dam gi dep bang sen\n");
+	return 0;
+}
