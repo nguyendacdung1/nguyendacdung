532d55c38da58e62995b620a4c2d4be934726e67
nguyendacdung1 <91303703+nguyendacdung1@users.noreply.github.com>
2021-09-29 11:31:57 +0700

    Add files via upload

---
+++ bai1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("class : T1104L\n");
+	printf("ro1no : b0001\n");
+	printf("Full Name : Nguyen Van A\n");
+	printf("Email : anv@fpt.aptech.ac.vn\n");
+	printf("Phone : 0912345678\n");
+	
+	return 0;
+}
