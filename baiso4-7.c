7f59b68d13c2837caa2234f65eafcd7682b1f11d
nguyendacdung1 <91303703+nguyendacdung1@users.noreply.github.com>
2021-10-03 09:26:40 +0700

    Add files via upload

---
+++ baiso4-7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int luong;
+	char loai;
+	printf("\nNhap muc luong: ");
+	scanf("%d", &luong);
+	printf("\nNhap loai nhan vien: ");
+	scanf("%c", &loai);
+
+	
+	return 0;
+}
+
