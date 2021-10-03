0463be020825f93487a9793b013076b6e3488a71
nguyendacdung1 <91303703+nguyendacdung1@users.noreply.github.com>
2021-10-03 09:21:51 +0700

    Add files via upload

---
+++ example8.2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int Num1, Num2;
+	clrscr();
+	Num1 = 540;
+	Num2 = 243;
+	if( Num1 > Num2)
+	printf("\n the greater number is: %d", Num1);
+	else
+	printf("\n the greater number is: %d", Num2);
+	return 0;
+}
