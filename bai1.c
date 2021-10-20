d525c3ccac9c79a0e84ecfb30c51dd226e9c57bc
nguyendacdung1 <91303703+nguyendacdung1@users.noreply.github.com>
2021-10-20 10:04:34 +0700

    Add files via upload

--- bai1.c
+++ bai1.c
@@ -4,11 +4,44 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("class : T1104L\n");
-	printf("ro1no : b0001\n");
-	printf("Full Name : Nguyen Van A\n");
-	printf("Email : anv@fpt.aptech.ac.vn\n");
-	printf("Phone : 0912345678\n");
-	
+	int a,b;
+	char chon;
+	printf("\nnhap so1:");
+	scanf("%d",&a);
+	printf("nhap so 2: ");
+	scanf("%d",&b);
+	printf("chon phep tinh:");
+	fflusf(stdin);
+	scanf("%c",&chon);
+	switch(chon)
+	{
+		case '+':
+			printf("tong:%d", tong(a,b));
+			break;
+		case '-':
+			printf("hieu:%d",hieu(a,b));
+			break;
+		case '*':
+			printf("tich:%d", tich(a,b));
+			break;
+		case '/':
+			printf("thuong:%d", thuong(a,b));
+			break;	
+		default:
+		   printf("khong dung");
+		   break;			
+	}
 	return 0;
+}
+int tong(int a,int b){
+	return a+b;
+}
+int hieu(int a,int b){
+	return a-b;
+}
+int tich(int a,int b){
+	return a*b;
+}
+int thuong(int a,int b){
+	return a/b;
 }
